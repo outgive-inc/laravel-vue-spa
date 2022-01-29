4eb5e50c3ec7ea71935def2c7d2ad7e1296d3989
Umang Patel <umangpatel1991@gmail.com>
2022-01-28 19:09:54 -0500

    create store for notes

---
+++ resources/js/store/modules/notes.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const state = {
+  notes: []
+};
+
+const getters = {
+  allNotes: state => state.notes
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
